223e35aa7186bc823240d651b67ad7c0a280222b
unmeshsharma2024 <unmeshsharma.25@gmail.com>
2025-10-26 14:28:17 -0700

    Update index.html to include Oswald font, add new SVG assets for firewall, endpoint, cloud, ADR, WAF, and robot scene, remove unused video file, and implement UnifiedSolution component in HomePage with corresponding styles.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { LandingHero } from '../components/HomeComponents/LandingHero';
+import { UnifiedSolution } from '../components/HomeComponents/UnifiedSolution';
 
 export default function HomePage() {
   return (
     <>
       <LandingHero />
+      <UnifiedSolution />
     </>
   );
 }
