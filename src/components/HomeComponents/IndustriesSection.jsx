5b06fc99793c363d716312ff3a8cd8212ff32e1a
unmeshsharma2024 <unmeshsharma.25@gmail.com>
2025-10-31 18:04:48 -0700

    Remove hover arrow animation from IndustriesSection component for a cleaner design. Simplify the layout by eliminating unnecessary motion effects.

--- src/components/HomeComponents/IndustriesSection.jsx
+++ src/components/HomeComponents/IndustriesSection.jsx
@@ -194,28 +194,6 @@ export function IndustriesSection() {
                     <p className="industry-card__description">
                       {industry.description}
                     </p>
-
-                    {/* Hover Arrow */}
-                    <motion.div
-                      className="industry-card__cta"
-                      initial={{ x: -10 }}
-                      whileHover={{ x: 0 }}
-                    >
-                      <span>Learn More</span>
-                      <svg
-                        className="industry-card__arrow"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M17 8l4 4m0 0l-4 4m4-4H3"
-                        />
-                      </svg>
-                    </motion.div>
                   </div>
 
                   {/* Decorative corner element */}
